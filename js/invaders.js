aeec225054da042cb426853e095bd3741779c5ed
Victor Dozal <v.dozal@live.com>
2017-05-05 20:44:53 -0500

    Added initial phaser JS

---
+++ js/invaders.js
@@ -0,0 +1,27 @@
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+    game.load.image('bullet', 'assets/games/invaders/bullet.png');
+    game.load.image('enemyBullet', 'assets/games/invaders/enemy-bullet.png');
+    game.load.spritesheet('invader', 'assets/games/invaders/invader32x32x4.png', 32, 32);
+    game.load.image('ship', 'assets/games/invaders/player.png');
+    game.load.spritesheet('kaboom', 'assets/games/invaders/explode.png', 128, 128);
+    game.load.image('starfield', 'assets/games/invaders/starfield.png');
+    game.load.image('background', 'assets/games/starstruck/background2.png');
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
+
+
+function render() {
+
+}
